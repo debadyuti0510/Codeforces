3f75b958cea04f6e750e8f0cc470af20900a5846
debadyuti0510 <36818630+debadyuti0510@users.noreply.github.com>
2018-03-05 19:35:22 +0530

    Add files via upload

---
+++ 266B.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	
+	int n,t;
+	cin>>n>>t;
+	string s;
+	cin>>s;
+	while(t>0)
+	{
+		for(int i=0; i<n; i++)
+		if(s[i]=='B' && s[i+1]=='G')
+		{
+			char t =s[i];
+			s[i]=s[i+1];
+			s[i+1]=t;
+			i++;
+		}
+		t--;
+	}
+	cout<<s;
+		 
+}
